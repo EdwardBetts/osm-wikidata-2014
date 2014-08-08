d1c3c44bfc1fa579dcb3bb0d1bff94a31c0683fc
Edward Betts <edwardbetts@gmail.com>
2014-08-08 13:16:11 +0100

    Add HTML candidate output.

--- check_candidates.py
+++ check_candidates.py
@@ -1,3 +1,6 @@
+import codecs
+
+
 def name_match(osm, wd):
     wd_lc = wd.lower()
     osm_lc = osm.lower()
@@ -11,11 +14,38 @@ def name_match(osm, wd):
         return True
     return False
 
+def find_matches():
+    for line in open('candidates'):
+        wikidata, osm = eval(line)
+        wd_name = wikidata['name']
+        maybe = [(i['id'], i['tags']['name']) for i in osm
+                 if name_match(i['tags']['name'], wd_name)]
+        if maybe:
+            yield (wikidata, maybe)
 
-for line in open('candidates'):
-    wikidata, osm = eval(line)
-    wd_name = wikidata['name']
-    maybe = [(i['id'], i['tags']['name']) for i in osm
-             if name_match(i['tags']['name'], wd_name)]
-    if maybe:
-        print (wikidata['id'], wd_name, maybe)
+def as_table():
+    out = codecs.open('matches.html', 'w', 'utf-8')
+    print >> out, u'<html><head>'
+    print >> out, u'<title>Wikidata to OSM matches</title>'
+    print >> out, u'<style>th { text-align: left; }</style>'
+    print >> out, u'</head>'
+    print >> out, u'<body>'
+    print >> out, u'<table>'
+    print >> out, u'<tr><th>Wikidata</th><th>OpenStreetMap</th></tr>'
+    for wikidata, osm in find_matches():
+        print >> out, u'<tr>'
+        print >> out, u'<td><a href="https://www.wikidata.org/wiki/{}">{}</a> ({})</td>'.format(
+              wikidata['id'], wikidata['name'], wikidata['id'])
+        osm0 = osm[0]
+        print >> out, u'<td><a href="http://www.openstreetmap.org/way/{}">{}</a> ({})</td>'.format(
+              osm0[0], osm0[1], osm0[0])
+        print >> out, u'</tr>'
+        if len(osm) > 1:
+            for i in osm[1:]:
+                print >> out, u'<tr><td></td>'
+                print >> out, u'<td><a href="http://www.openstreetmap.org/way/{}">{}</a> ({})</td>'.format(
+                      i[0], i[1], i[0])
+                print >> out, u'</tr>'
+    print >> out, u'</table></body></html>'
+
+as_table()
